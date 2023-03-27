0aab6a41d9e9b420292e709273e7824e94f61072
Vaani-Arora <115731505+Vaani-Arora@users.noreply.github.com>
2023-03-27 16:10:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.all('*', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://imdb-vaani.netlify.app/");
+  res.header("Access-Control-Allow-Origin", "https://imdb-vaani.netlify.app");
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Credentials', true);
